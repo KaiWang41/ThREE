ba71ea1e486ab6ff93ca67bf9168bca764d73c8b
Kangyi Wang <164071754@qq.com>
2018-08-23 13:46:09 +1000

    Update tree names

---
+++ ThREE/ThREE/FirstViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FirstViewController.swift
+//  ThREE
+//
+//  Created by wky on 23/8/18.
+//  Copyright © 2018 ThREE. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+
+}
+
---
+++ ThREE/ThREE/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ThREE
+//
+//  Created by wky on 23/8/18.
+//  Copyright © 2018 ThREE. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
