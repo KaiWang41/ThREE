de1926124da9382f157c27fd72be7218b8fe2b90
Kangyi Wang <164071754@qq.com>
2018-09-11 15:35:19 +1000

    Add detect text and scale

--- App/App/FirstViewController.swift
+++ App/App/FirstViewController.swift
@@ -34,6 +34,13 @@ class FirstViewController: UIViewController, UIImagePickerControllerDelegate, UI
     var clfLabel = [""]
     var clfConf = [Float(0)]
     
+    // Size calculation.
+    var pixelCounts = [0]
+    
+    // Text board size in metres.
+    let boardWidth: CGFloat = 1.1
+    let boardHeight: CGFloat = 0.3
+    
     
     
     
@@ -63,7 +70,7 @@ class FirstViewController: UIViewController, UIImagePickerControllerDelegate, UI
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { _ in
             
-            // Find images and classify.
+            // Find images and process.
             var n = 0
             for subview in self.stackView.arrangedSubviews {
                 if n == self.currentPhotoNum {
@@ -72,7 +79,8 @@ class FirstViewController: UIViewController, UIImagePickerControllerDelegate, UI
                 if type(of: subview) == UIImageView.self {
                     
                     let image = (subview as! UIImageView).image
-                    self.classify(image: image!)
+//                    self.classify(image: image!)
+                    self.calculate(image: image!)
                     n += 1
                 }
             }
@@ -84,25 +92,25 @@ class FirstViewController: UIViewController, UIImagePickerControllerDelegate, UI
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        print(clfLabel, "\n", clfConf)
-        if segue.identifier == "toTreeResults" {
-            if let des = segue.destination as? UINavigationController {
-                if let vc = des.topViewController as? TreeResultsViewController {
-                    
-                    var maxConf = Float(-1)
-                    var type = ""
-                    for i in 1...(clfConf.count-1) {
-                        if clfConf[i] > maxConf {
-                            maxConf = clfConf[i]
-                            type = clfLabel[i]
-                        }
-                    }
-                    
-                    vc.typeText = type
-                    vc.confText = String(Int(maxConf*100))+"%"
-                }
-            }
-        }
+
+//        if segue.identifier == "toTreeResults" {
+//            if let des = segue.destination as? UINavigationController {
+//                if let vc = des.topViewController as? TreeResultsViewController {
+//
+//                    var maxConf = Float(-1)
+//                    var type = ""
+//                    for i in 1...(clfConf.count-1) {
+//                        if clfConf[i] > maxConf {
+//                            maxConf = clfConf[i]
+//                            type = clfLabel[i]
+//                        }
+//                    }
+//
+//                    vc.typeText = type
+//                    vc.confText = String(Int(maxConf*100))+"%"
+//                }
+//            }
+//        }
     }
     
     // Remove all current photos.
@@ -130,6 +138,8 @@ class FirstViewController: UIViewController, UIImagePickerControllerDelegate, UI
         }))
         present(ac, animated: true)
     }
+    
+    
     
     // MARK: - Privates
     
@@ -337,7 +347,6 @@ class FirstViewController: UIViewController, UIImagePickerControllerDelegate, UI
                 fatalError("Failed to load results.")
             }
             
-            print(results)
             if let topResult = results.first {
                 self.clfLabel.append(topResult.identifier)
                 self.clfConf.append(topResult.confidence)
@@ -350,13 +359,161 @@ class FirstViewController: UIViewController, UIImagePickerControllerDelegate, UI
             try handler.perform([request])
         }
         catch {
-            let ac = UIAlertController(title: "Error", message: error.localizedDescription + "\nPlease try again.", preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "OK", style: .destructive, handler: nil))
-            present(ac, animated: true)
+            self.presentError(error.localizedDescription + "\nPlease try again.")
         }
     }
     
     
+    
+    
+    
+    
+    
+    
+    // Mark: - Size Calculation
+    
+    /// - Tag: PreprocessImage
+    func scaleAndOrient(image: UIImage) -> UIImage {
+        
+        // Set a default value for limiting image size.
+        let maxResolution: CGFloat = 640
+        
+        guard let cgImage = image.cgImage else {
+            print("UIImage has no CGImage backing it!")
+            return image
+        }
+        
+        // Compute parameters for transform.
+        let width = CGFloat(cgImage.width)
+        let height = CGFloat(cgImage.height)
+        var transform = CGAffineTransform.identity
+        
+        var bounds = CGRect(x: 0, y: 0, width: width, height: height)
+        
+        if width > maxResolution ||
+            height > maxResolution {
+            let ratio = width / height
+            if width > height {
+                bounds.size.width = maxResolution
+                bounds.size.height = round(maxResolution / ratio)
+            } else {
+                bounds.size.width = round(maxResolution * ratio)
+                bounds.size.height = maxResolution
+            }
+        }
+        
+        let scaleRatio = bounds.size.width / width
+        let orientation = image.imageOrientation
+        switch orientation {
+        case .up:
+            transform = .identity
+        case .down:
+            transform = CGAffineTransform(translationX: width, y: height).rotated(by: .pi)
+        case .left:
+            let boundsHeight = bounds.size.height
+            bounds.size.height = bounds.size.width
+            bounds.size.width = boundsHeight
+            transform = CGAffineTransform(translationX: 0, y: width).rotated(by: 3.0 * .pi / 2.0)
+        case .right:
+            let boundsHeight = bounds.size.height
+            bounds.size.height = bounds.size.width
+            bounds.size.width = boundsHeight
+            transform = CGAffineTransform(translationX: height, y: 0).rotated(by: .pi / 2.0)
+        case .upMirrored:
+            transform = CGAffineTransform(translationX: width, y: 0).scaledBy(x: -1, y: 1)
+        case .downMirrored:
+            transform = CGAffineTransform(translationX: 0, y: height).scaledBy(x: 1, y: -1)
+        case .leftMirrored:
+            let boundsHeight = bounds.size.height
+            bounds.size.height = bounds.size.width
+            bounds.size.width = boundsHeight
+            transform = CGAffineTransform(translationX: height, y: width).scaledBy(x: -1, y: 1).rotated(by: 3.0 * .pi / 2.0)
+        case .rightMirrored:
+            let boundsHeight = bounds.size.height
+            bounds.size.height = bounds.size.width
+            bounds.size.width = boundsHeight
+            transform = CGAffineTransform(scaleX: -1, y: 1).rotated(by: .pi / 2.0)
+        }
+        
+        return UIGraphicsImageRenderer(size: bounds.size).image { rendererContext in
+            let context = rendererContext.cgContext
+            
+            if orientation == .right || orientation == .left {
+                context.scaleBy(x: -scaleRatio, y: scaleRatio)
+                context.translateBy(x: -height, y: 0)
+            } else {
+                context.scaleBy(x: scaleRatio, y: -scaleRatio)
+                context.translateBy(x: 0, y: -height)
+            }
+            context.concatenate(transform)
+            context.draw(cgImage, in: CGRect(x: 0, y: 0, width: width, height: height))
+        }
+    }
+    
+    func calculate(image: UIImage) {
+        
+        // Convert from UIImageOrientation to CGImagePropertyOrientation.
+        let cgOrientation = CGImagePropertyOrientation(image.imageOrientation)
+        
+        guard let cgImage = image.cgImage else {
+            fatalError("The image has no CGImage.")
+        }
+        
+        let request = VNDetectTextRectanglesRequest(completionHandler: { (request, error) in
+            
+            guard let results = request.results as? [VNTextObservation] else {
+                fatalError("VN text observation error.")
+            }
+            
+            // Make sure there is only one text observation with 5 characters.
+       
+            var count = 0
+            var targetObservation: VNTextObservation?
+            for result in results {
+                if let n = result.characterBoxes?.count {
+              
+                    if n == 5 {
+                        count += 1
+                        targetObservation = result
+                    }
+                }
+            }
+            if count < 1 {
+                self.presentError("Failed to detect text board. Please try and make it clearly visible.")
+                return
+            }
+            if count > 1 {
+                self.presentError("Failed to detect text board. Please try and avoid other texts in the photo.")
+                return
+            }
+            
+            // Get text observation bounding and scale.
+            if let box = targetObservation?.boundingBox {
+                
+                // Image size in metres.
+                
+                let imageWidthMeters = self.boardWidth / box.size.width
+                let imageHeightMeters = self.boardHeight / box.size.height
+                
+            }
+        })
+        request.reportCharacterBoxes = true
+        
+        let handler = VNImageRequestHandler(cgImage: cgImage, orientation: cgOrientation)
+        
+        do {
+            try handler.perform([request])
+        }
+        catch {
+            self.presentError(error.localizedDescription + "\nPlease try again.")
+        }
+    }
+    
+    private func presentError(_ message: String) {
+        let ac = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .destructive, handler: nil))
+        present(ac, animated: true)
+    }
 
     
 }
